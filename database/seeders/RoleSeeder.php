4391e0f8fd04c76561d7756a47fca39f6bb2789b
ACH RIZAL <achrizal15@gmail.com>
2022-11-15 22:40:48 +0700

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,7 +15,7 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::create([
-            "name"=>"IT Support",
+            "name"=>"admin",
             "landing_page"=>"/home",
         ]);
         Role::create([
